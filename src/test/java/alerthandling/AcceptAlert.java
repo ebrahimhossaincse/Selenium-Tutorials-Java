83cffc9a59c61c90b8efd55bd8873997b2e510ee
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-03-28 05:29:34 -0400

    alert - accept

---
+++ src/test/java/alerthandling/AcceptAlert.java
@@ -0,0 +1,45 @@
+package alerthandling;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class AcceptAlert {
+	protected static String url = "https://demoqa.com/alerts";
+	WebDriver driver;
+
+	@BeforeSuite
+	public void startChromeBrowser() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+
+	@BeforeClass
+	public void openUrl() {
+		driver.get(url);
+	}
+
+	@Test(priority = 0)
+	public void acceptAlert() throws InterruptedException {
+		WebElement alertButton = driver.findElement(By.xpath("//button[@id='alertButton']"));
+		alertButton.click();
+		Thread.sleep(2000);
+		
+		Alert alert = driver.switchTo().alert();
+		alert.accept();
+		Thread.sleep(3000);
+	}
+
+	@AfterSuite
+	public void closeChromeBrowser() {
+		driver.quit();
+	}
+}
