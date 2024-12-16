f3d1748a84fee4929aad6bb96f70da70041e17e7
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-12-16 05:16:17 -0500

    update

--- src/test/java/browserhanding/ChromeBrowserInSelenium.java
+++ src/test/java/browserhanding/ChromeBrowserInSelenium.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class ChromeBrowserInSelenium {
-	protected static String url = "https://www.testingtherapy.com/";
+	protected static String url = "https://www.google.com/";
 	WebDriver driver;
 
 	@BeforeSuite
